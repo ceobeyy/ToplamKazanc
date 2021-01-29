b5cca0cf1828e10fe4ee642c2c4ad2d3323ced31
Ömer Turan <77924528+ceobeyy@users.noreply.github.com>
2021-01-29 19:06:17 +0300

    Add files via upload

---
+++ ToplamKazanc.sql
@@ -0,0 +1,3 @@
+﻿select ProductName as ÜrünAdi,sum(od.UnitPrice*od.Quantity) as ToplamKazanç
+from Products as p inner join [Order Details] as od on p.ProductID = od.ProductID
+inner join Orders as o on o.OrderID = od.OrderID group by ProductName
